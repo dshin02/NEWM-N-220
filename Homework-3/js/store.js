340366cfc127094754f0d98ba053cfae5950e497
Daniel Shin <danhshin@iu.edu>
2025-02-04 16:09:06 -0500

    js file should be linked now

--- Homework-3/js/store.js
+++ Homework-3/js/store.js
@@ -1,10 +1,17 @@
 const itemPrice = 40.99; // Constant as item price does not change
+
 const salesTax = 0.07;
+
 const discountRate = 0.10;
+
 const totalAfterTax = itemPrice * (1 + salesTax);
+
 const qualifiesForDiscount = itemPrice > 50;
-const discountedTotalAfterTax = qualifiesForDiscount ? totalAfterTax * (1 - discountRate) : totalAfterTax;
+
+const discountedTotalAfterTax = totalAfterTax * (qualifiesForDiscount * (1 - discountRate) + !qualifiesForDiscount);
 
 console.log("Total After Tax: " + totalAfterTax);
+
 console.log("Qualifies for Discount: " + qualifiesForDiscount);
+
 console.log("Discounted Total After Tax: " + discountedTotalAfterTax);
