6881dcb09357c2ea3da3c0100bcdd8e4a227ae2d
Daniel Shin <danhshin@iu.edu>
2025-02-19 14:06:02 -0500

    addded readme

--- Homework-4/js/gallery.js
+++ Homework-4/js/gallery.js
@@ -9,7 +9,7 @@ const gallerySection = document.getElementById("gallery-section");
 
 let galleryImages = "";
 for (let i = 0; i < photos.length; i++) {
-    galleryImages += "<img src=\"" + photos[i] + "\" alt=\"Image " + (i + 1) + "\" height=\"300\">";
+    galleryImages += "<img src=\"" + photos[i] + "\" alt=\"#Image " + (i + 1) + "\" height=\"300\">";
 }
 
 gallerySection.innerHTML = galleryImages;
